00f002bdd2b1a62f08e383d92e62541d97bd4902
xueting <wanxueting@dailyyoga.com>
2023-01-18 09:09:09 +0800

    Initial Commit

---
+++ SafioIsland/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SafioIsland
+//
+//  Created by Safio on 2023/1/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SafioIsland/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SafioIsland
+//
+//  Created by Safio on 2023/1/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
